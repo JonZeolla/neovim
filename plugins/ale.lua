64557e8b6b386a26410c43a011b20450f3aa432b
Jon Zeolla <zeolla@gmail.com>
2024-05-02 16:25:22 -0400

    Turn off ALE linters

--- plugins/ale.lua
+++ plugins/ale.lua
@@ -2,7 +2,12 @@ local plugin = {
   "dense-analysis/ale",
   lazy = false,
   config = function()
+    local b = vim.b
     local g = vim.g
+
+    -- This turns off the ALE linters, which are on by default
+    -- I only use ALE for the fixers; everything else should be in lspconfig or something else
+    b.ale_linters = {}
 
     g.ale_completion_enabled = 1
     g.ale_fixers = {
