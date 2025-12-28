5b633aa5a0f8fd9b94f672ed237612260b3984b2
Jon Zeolla <zeolla@gmail.com>
2025-12-28 10:40:54 -0500

    Remove conflict

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,10 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- LazyVim extras (AI)
-    { import = "lazyvim.plugins.extras.ai.copilot" },
-    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
-    -- LazyVim extras (DAP)
+    -- LazyVim extras
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
     -- User plugins
