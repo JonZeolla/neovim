1fd4b95ca937ac7c5a22cc3c1f1ef8355cd63e9e
Jon Zeolla <zeolla@gmail.com>
2025-12-28 10:30:31 -0500

    Use ruff to orgnaize imports

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -7,6 +7,7 @@ return {
       formatters_by_ft = {
         ["*"] = { "trim_whitespace", "trim_newlines" },
         go = { "gofmt" },
+        python = { "ruff_organize_imports", "ruff_format" },
         rust = { "rustfmt" },
         terraform = { "terraform_fmt" },
       },
